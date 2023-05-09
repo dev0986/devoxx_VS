fc26249b355871a95844067cfe1857f20eb76984
atish.gurumurthi <atish.gurumurthi@gmail.com>
2023-05-08 20:51:01 -0400

    Lambda - commit - 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,37 @@ package org.example;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+//Invoked Dynamic
+// When anonymous classes are used to implement Functional interfaces, .class files are created.
+//Lambda usage for Functional interfaces implementation -> no .class/ anonymous classes created.
+//No garbage collection extra.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Devoxx - Venkat S");
+
+        //Function
+        //name - not important
+        //parameter list // Lambda expression
+        //body
+        // return type - can be inferred
+
+
+        Thread lambdathread =
+                new Thread(() ->
+                        System.out.println("Hello world"));
+        lambdathread.start();
+
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("In another thread...");
+            }
+        });
+        th.start();
+
+//        Thread th1 = new Thread();
+//        th1.start();
+        System.out.println("In main");
 
     }
 }
---
+++ src/main/java/org/example/MainLambda.java
@@ -0,0 +1,72 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class MainLambda {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        //println is an instance method on System.out (not a static method)
+//        numbers.forEach(e -> System.out.println(e));
+//        numbers.forEach(System.out::println);
+        //Method ref to an instance method
+
+
+        //Argument to a static method
+//        numbers.stream()
+//               // .map(e -> String.valueOf(e))
+//                //reference to a static method
+//                .map(String::valueOf)
+//                .forEach(System.out::println);
+
+
+        numbers.stream()
+                .map(e -> String.valueOf(e))
+                //.map(e -> e.toString())
+                .map(String::toString)
+                .forEach(System.out::println);
+
+        //external iterators # 1
+        for (int i = 0; i < numbers.size(); i++) { //controlling the iteration
+            //System.out.println(numbers.get(i));
+        }
+
+        //external iterator # 2
+        for (int e : numbers) { //controlling the iteration
+            //System.out.println(e);
+        }
+
+        //internal iterator
+        //pass object to a functions vs calling a function using an object (polymorphism)
+        numbers.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                //System.out.print(integer);
+            }
+        });
+        //Java 8 has type inference, finally,
+        //hold your tweets, but only for lambda expressions
+        //paranthesis is optional but only for one parameter lambdas.
+        //numbers.forEach(e -> System.out.print(e));
+        //Method reference
+        //numbers.forEach(System.out::println);
+
+        //While Lambdas are really cute, keep them that way.
+
+        //Method references: Only when you want to pass what you get onto the next journey
+        //No manipulation/ business logic
+
+
+
+
+
+
+
+
+
+    }
+}
